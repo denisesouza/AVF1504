8e3905c2b0761455eb2e70eb57da5c38df4bfcb5
DSouza <drsouza@fullsail.edu>
2015-04-04 20:35:24 -0400

    Add code to titanium, debug code

--- Souza_Denise_WK1Project_WeatherApp/Resources/app.js
+++ Souza_Denise_WK1Project_WeatherApp/Resources/app.js
@@ -1,64 +1,22 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
+/**
+ * @author Denise Souza
+ * AVF1504 Weather App
+ */
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+//background
+Titanium.UI.setBackgroundColor ("#000");
+
+//set var for geo and call / require .js files
+var ui = require("ui");
+var geo = require("geo");
+var loadWeatherData = require("weatherData");
 
 
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+//add event listener for button that prompts to allow location
+ui.button.addEventListener("click", function(event) {
+	geo.geo(); //will run the geo function once clicked
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
 
 
 
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
